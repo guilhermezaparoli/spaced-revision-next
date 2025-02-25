68b4c45a0e2ccce9ff38bcec09e975c0140d1b23
Guilherme Zaparoli Gomes <guilhermezapas@gmail.com>
2025-02-25 07:09:30 -0300

    Adding shadcn accordion and avatar

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,6 +10,10 @@ export default {
   theme: {
   	extend: {
   		colors: {
+			accordion: "#1E293B",
+			rowAccordion: "#253143",
+			white: "#E5E7EB",
+			darkbg: "#0F172A",
   			background: 'hsl(var(--background))',
   			foreground: 'hsl(var(--foreground))',
   			card: {
@@ -55,6 +59,28 @@ export default {
   			lg: 'var(--radius)',
   			md: 'calc(var(--radius) - 2px)',
   			sm: 'calc(var(--radius) - 4px)'
+  		},
+  		keyframes: {
+  			'accordion-down': {
+  				from: {
+  					height: '0'
+  				},
+  				to: {
+  					height: 'var(--radix-accordion-content-height)'
+  				}
+  			},
+  			'accordion-up': {
+  				from: {
+  					height: 'var(--radix-accordion-content-height)'
+  				},
+  				to: {
+  					height: '0'
+  				}
+  			}
+  		},
+  		animation: {
+  			'accordion-down': 'accordion-down 0.2s ease-out',
+  			'accordion-up': 'accordion-up 0.2s ease-out'
   		}
   	}
   },
